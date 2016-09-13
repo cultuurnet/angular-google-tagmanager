565379390a16e37469cf1d9ed6cb43743cb0269d
zuuperman <nils.destoop@gmail.com>
2016-09-13 18:38:04 +0200

    Make sure unit tests don't break if they don't have a document

--- src/cultuurnet-google-tagmanager.module.js
+++ src/cultuurnet-google-tagmanager.module.js
@@ -29,12 +29,16 @@ function initGaTagManager(window, document, containerId) {
   document.ready(function () {
     (function (w, d, s, l, i) {
       w[l] = w[l] || []; w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
-      var f = d.getElementsByTagName(s)[0],
-        j = d.createElement(s),
-        dl = l !== 'dataLayer' ? '&l=' + l : '';
-      j.async = true;
-      j.src = '//www.googletagmanager.com/gtm.js?id=' + i + dl;
-      f.parentNode.insertBefore(j, f);
+
+      if (d) {
+        var f = d.getElementsByTagName(s)[0],
+          j = d.createElement(s),
+          dl = l !== 'dataLayer' ? '&l=' + l : '';
+        j.async = true;
+        j.src = '//www.googletagmanager.com/gtm.js?id=' + i + dl;
+        f.parentNode.insertBefore(j, f);
+      }
+
     })(window, document[0], 'script', 'tm', containerId);
   });
 }
